56d0673d954a6a5275a1cdb3afba078cd30e5bb3
Anirudh Pai <apai25@berkeley.edu>
2025-05-03 22:47:17 -0700

    unfreeze ds

--- src/datasets/droid_dataset.py
+++ src/datasets/droid_dataset.py
@@ -85,7 +85,7 @@ class DroidDatasetIndexed(Dataset):
         return len(self.samples)
 
     def __getitem__(self, idx):
-        sample = self.samples[0]
+        sample = self.samples[idx]
 
         context_obs = torch.stack(
             [
