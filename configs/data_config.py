efd5158882a444a55669241a5b9409cda3a93d78
Ryan Cheng <ryancheng@berkeley.edu>
2025-05-12 22:25:55 -0700

    flow matching difference and pretrained wrist gifs

--- configs/data_config.py
+++ configs/data_config.py
@@ -9,8 +9,8 @@ class DataConfig:
     # data_dir: Path = Path("/nfs/kun2/datasets").resolve()
     dataset_name: str = "droid_100"
     # dataset_name: str = "droid"
-    # camera: str = "wrist_image_left"
-    camera: str = "exterior_image_1_left"
+    camera: str = "wrist_image_left"
+    # camera: str = "exterior_image_1_left"
     horizon: int = 5
 
     action_dim: int = 7
